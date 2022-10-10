db78216d2a9012cd58a47a22128bc0a89e4fbc33
ThatKirbyGuy <ThatKirbyGuy25@gmail.com>
2022-10-10 19:19:14 +0300

    Добавьте файлы проекта.

---
+++ CylinderVolumeGeometry/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CylinderVolumeGeometry")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("CylinderVolumeGeometry")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ea75b5d1-f98e-42a7-91b0-670e8f4df8fe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CylinderVolumeGeometry/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CylinderVolume;
+
+namespace CylinderVolumeGeometry
+{
+    [TestClass]
+    public class CylinderGeometry
+    {
+        [TestMethod]
+        public void CylinderVolume_return()
+        {
+            // исходные данные
+            int S = 7;
+            int h = 8;
+            int V = 56;
+
+            // получение значения с помощью тестируемого метода
+            Geometry g = new Geometry();
+            int actual = g.CylinderVolume(S, h);
+
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(V, actual);
+
+        }
+    }
+}
